67d2d65e607ed9ead532d0208bba0f3b986394da
HumayunKabir1981 <callmilton.com>
2026-05-19 21:55:53 +0600

    car Add insert rout done

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const dotenv = require('dotenv')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors')
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    await client.connect();
+    const db = client.db("drivenow")
+    const addcarCollection = db.collection("addcar")
+
+
+
+    app.get('/addcar', async (req, res) => {
+      const result = await addcarCollection.find().toArray()
+      res.json(result)
+
+    })
+
+
+    app.post('/addcar', async (req, res) => {
+      const addcarData = req.body;
+      console.log(addcarData);
+      
+      const result = await addcarCollection.insertOne(addcarData);
+
+      res.json(result);
+    })
+
+
+    // app.get('/destination/:id', async (req, res) => {
+    //   const { id } = req.params
+    //   const result = await destinationCollection.findOne({ _id: new ObjectId(id) })
+    //   res.json(result)
+    // })
+
+    // app.patch('/destination/:id', async (req, res) => {
+    //   const { id } = req.params
+    //   const updateData = req.body;
+    //   console.log(updateData);
+    //   const result = await destinationCollection.updateOne(
+    //     { _id: new ObjectId(id) },
+    //     { $set: updateData }
+    //   )
+    //   res.json(result)
+    // })
+
+    // app.delete('/destination/:id', async (req, res) => {
+    //   const { id } = req.params
+    //   const result = await destinationCollection.deleteOne({ _id: new ObjectId(id) })
+
+    //   res.json(result)
+    // })
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+  res.send("Server is Running Fine")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`);
+
+})
